0910c61cefb4f6aa02651359af47b559b3a6a38b
tranzitivnost <alevtinapatkina@gmail.com>
2025-04-01 16:17:58 +0200

    added new changes to ContactPage component

--- src/pages/contact/ui/ContactPage.tsx
+++ src/pages/contact/ui/ContactPage.tsx
@@ -1,30 +1,18 @@
 import { Container } from "@shared/ui";
-import { PageFooter, PageHeader } from "@/widgets/page";
+import { PageHeader } from "@/widgets/page";
+import { Image } from "@/shared/ui";
 import styles from "./ContactPage.module.css";
-import { usePopularMovies } from "@entities/popular-movies";
+import Poster from "@/shared/ui/assets/poster.png";
+
 import { ContactInformation } from "@/widgets/information";
 
-import { Divider } from "@shared/ui";
-
 export function ContactPage() {
-  const { popularMovies } = usePopularMovies({
-    page: 1,
-    sort_by: "vote_count.desc",
-    primary_release_year: 2025,
-  });
-  const firstMovie = popularMovies?.[0];
-
   return (
-    <Container
-      container
-      flexDirectionColumn
-      justifyBetween
-      alignCenter
-      className={styles.container}
-    >
-      <PageHeader />
-      <ContactInformation />
-      <Divider />
+    <Container container flexDirectionColumn justifyBetween alignCenter>
+      <PageHeader positionAbsolute />
+      <Container className={styles.gradient} />
+      <Image className={styles["movie-poster"]} src={Poster} alt="poster" />
+      <ContactInformation className={styles.information} />
     </Container>
   );
 }
--- src/widgets/information/ui/ContactInformation.tsx
+++ src/widgets/information/ui/ContactInformation.tsx
@@ -1,9 +1,17 @@
 import { Container, Header } from "@shared/ui";
 import styles from "./ContactInformation.module.css";
-import { Link } from "@shared/ui";
 import { Text } from "@shared/ui";
+import clsx from "clsx";
 
-export function ContactInformation() {
+interface Props
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  className?: string;
+}
+
+export function ContactInformation({ className }: Props) {
   return (
     <Container
       alignStart
@@ -11,61 +19,51 @@ export function ContactInformation() {
       justifyBetween
       flexDirectionColumn
       gap="16px"
-      className={styles.container}
+      className={clsx([styles.container, className])}
     >
+      <Header className={styles.title}>
+        <p>Best site to watch movies around the world</p>
+      </Header>
       <Container
         container
         flexDirectionColumn
         justifyBetween
         alignStart
-        className={styles.titleContainer}
+        className={styles.information}
       >
-        <Header className={styles.title}>
-          <p>Best site to watch movies around the world</p>
-        </Header>
-        <Container
-          container
-          flexDirectionColumn
-          justifyBetween
-          alignStart
-          className={styles.information}
-        >
-          <Container container flexDirectionColumn justifyBetween alignStart>
-            <Header className={styles.phoneTitle}>CONTACT US</Header>
-            <Header className={styles.phone}>+381 98 765 4321</Header>
-          </Container>
-
-          <Container container flexDirectionColumn justifyBetween alignStart>
-            <Header className={styles.phoneTitle}>ADDRESS</Header>
-            <Header className={styles.phone}>
-              1071 Fifth Avenue at 89th Street Manhattan, New York City
-            </Header>
-          </Container>
-
-          <Container container flexDirectionColumn justifyBetween alignStart>
-            <Header className={styles.phoneTitle}>
-              YOUR QUESTIONS AND SPECIAL REQUESTS ARE ALWAYS WELCOME! 🚀🎉
-            </Header>
-            <Header className={styles.phone}>
-              saintstream_support@gmail.com
-            </Header>
-          </Container>
+        <Container container flexDirectionColumn justifyBetween alignStart>
+          <Header className={styles.phoneTitle}>CONTACT US</Header>
+          <Text className={styles.phone}>+381 98 765 4321</Text>
         </Container>
+
+        <Container container flexDirectionColumn justifyBetween alignStart>
+          <Header className={styles.phoneTitle}>ADDRESS</Header>
+          <Text className={styles.phone}>
+            1071 Fifth Avenue at 89th Street Manhattan, New York City
+          </Text>
+        </Container>
+
+        <Container container flexDirectionColumn justifyBetween alignStart>
+          <Header className={styles.phoneTitle}>
+            YOUR QUESTIONS AND SPECIAL REQUESTS ARE ALWAYS WELCOME! 🚀🎉
+          </Header>
+          <Text className={styles.phone}>saintstream_support@gmail.com</Text>
+        </Container>
+      </Container>
+      <Container
+        container
+        justifyBetween
+        className={styles.secondTitleContainer}
+      >
         <Container
           container
           justifyBetween
-          className={styles.secondTitleContainer}
+          className={styles.anotherTitleContainer}
         >
-          <Container
-            container
-            justifyBetween
-            className={styles.anotherTitleContainer}
-          >
-            <Header className={styles.secondTitle}>Privacy policy</Header>
-            <Header className={styles.secondTitle}>Term of service</Header>
-          </Container>
-          <Header className={styles.secondTitle}>© 2025</Header>
+          <Header className={styles.secondTitle}>Privacy policy</Header>
+          <Header className={styles.secondTitle}>Term of service</Header>
         </Container>
+        <Header className={styles.secondTitle}>© 2025</Header>
       </Container>
     </Container>
   );
