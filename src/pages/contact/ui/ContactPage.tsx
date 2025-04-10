6eef8d544bbd28ccbc7495879175b35fae43c4ae
tranzitivnost <alevtinapatkina@gmail.com>
2025-04-10 14:50:48 +0200

    add some new changes to ContactPage's classes

--- src/pages/contact/ui/ContactPage.tsx
+++ src/pages/contact/ui/ContactPage.tsx
@@ -1,18 +1,20 @@
 import { Container } from "@shared/ui";
 import { PageHeader } from "@/widgets/page";
-import { Image } from "@/shared/ui";
 import styles from "./ContactPage.module.css";
-import Poster from "@/shared/ui/assets/poster.png";
 
 import { ContactInformation } from "@/widgets/information";
 
 export function ContactPage() {
   return (
-    <Container container flexDirectionColumn justifyBetween alignCenter>
+    <Container
+      container
+      flexDirectionColumn
+      justifyBetween
+      alignCenter
+      className={styles.container}
+    >
       <PageHeader positionAbsolute />
-      <Container className={styles.gradient} />
-      <Image className={styles["movie-poster"]} src={Poster} alt="poster" />
-      <ContactInformation className={styles.information} />
+      <ContactInformation />
     </Container>
   );
 }
