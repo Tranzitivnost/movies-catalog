952f2b05c1839b1682525f7e441680ecd11a7712
tranzitivnost <alevtinapatkina@gmail.com>
2025-04-11 17:15:43 +0200

    add new branch film_page

---
+++ src/pages/film/ui/FilmPage.tsx
@@ -0,0 +1,36 @@
+import { Container } from "@shared/ui";
+import { PageFooter, PageHeader } from "@/widgets/page";
+import styles from "./HomePage.module.css";
+import { usePopularMovies } from "@entities/popular-movies";
+import { PosterMovie } from "@/features/poster-movie";
+import { MoviesList } from "@/features/movies-list";
+import { Divider } from "@shared/ui";
+
+export function FilmPage() {
+  const currentYear = new Date().getFullYear();
+  const { popularMovies } = usePopularMovies({
+    page: 1,
+    sort_by: "vote_count.desc",
+    primary_release_year: currentYear,
+  });
+  const firstMovie = popularMovies?.[0];
+
+  return (
+    <Container
+      container
+      flexDirectionColumn
+      justifyBetween
+      alignCenter
+      className={styles.container}
+    >
+      <PageHeader positionAbsolute />
+      {firstMovie && (
+        <PosterMovie movie={firstMovie} className={styles.posterContainer} />
+      )}
+      <Divider />
+      <MoviesList title="Popular movies for you" movies={popularMovies} />
+      <Divider />
+      <PageFooter />
+    </Container>
+  );
+}
