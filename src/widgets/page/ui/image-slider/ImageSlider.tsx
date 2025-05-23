91ee4cd44c4be85f3a62a1eede88f01ce37f9fd6
tranzitivnost <alevtinapatkina@gmail.com>
2025-05-23 21:43:23 +0200

    added smth

--- src/widgets/page/ui/image-slider/ImageSlider.tsx
+++ src/widgets/page/ui/image-slider/ImageSlider.tsx
@@ -3,6 +3,7 @@ import { Container, Button } from "@/shared/ui";
 import { useState } from "react";
 import styles from "./ImageSlider.module.css";
 import clsx from "clsx";
+import { current } from "@reduxjs/toolkit";
 interface Props {
   children: (slide: Movie, index: number) => React.ReactNode;
   slides: Movie[];
@@ -16,15 +17,17 @@ export function ImageSlider({ slides, visibleCount = 3, children }: Props) {
   if (slides.length === 0) {
     return <p>Loading...</p>;
   }
-
-  const maxIndex = slides.length - visibleCount;
-
-  const next = () => {
-    setStartIndex(prev => Math.min(prev + 1, maxIndex));
+  const nextImageGenerate = () => {
+    const isLastSlide = startIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : startIndex + 1;
+    setStartIndex(newIndex);
   };
 
-  const prev = () => {
-    setStartIndex(prev => Math.max(prev - 1, 0));
+  const prevImageGenerate = () => {
+    const isFirstSlide = startIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : startIndex - 1;
+
+    setStartIndex(newIndex);
   };
 
   const visibleImages = slides.slice(startIndex, startIndex + visibleCount);
@@ -32,8 +35,7 @@ export function ImageSlider({ slides, visibleCount = 3, children }: Props) {
   return (
     <Container container alignCenter gap="12px" className={styles.container}>
       <Button
-        onClick={prev}
-        disabled={startIndex === 0}
+        onClick={prevImageGenerate}
         className={clsx([styles.button, styles.prevButton])}
       />
       <Container container gap="24px" className={styles.visibleImages}>
@@ -41,11 +43,7 @@ export function ImageSlider({ slides, visibleCount = 3, children }: Props) {
           return children(slide, index);
         })}
       </Container>
-      <Button
-        onClick={next}
-        disabled={startIndex >= maxIndex}
-        className={styles.button}
-      />
+      <Button onClick={nextImageGenerate} className={styles.button} />
     </Container>
   );
 }
