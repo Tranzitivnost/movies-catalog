0069f2e2402aedc13ae977036b8401edfeeb1072
sergeyteleshev <iamsergeyteleshev@gmail.com>
2025-07-27 22:25:44 +0200

    fixes build

--- src/entities/credits/index.ts
+++ src/entities/credits/index.ts
@@ -2,5 +2,5 @@ export { mapCastMembers } from "./model/mapCastMembers";
 export { movieCastSliceReducer } from "./model/creditsSlice";
 export { fetchMovieCredits } from "./model/creditsThunk";
 export { selectCastByMovieId } from "./model/selectors";
-export { useMovieCredits } from "./useMovieCredits";
+export { useMovieCredits } from "./usemovieCredits";
 export { CastMemberPreview } from "./ui/CastMemberPreview";
