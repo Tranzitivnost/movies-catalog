ef739b98bd512d4ac2acede4b702acc41473fc46
tranzitivnost <alevtinapatkina@gmail.com>
2026-02-24 18:48:28 +0100

    feat: enhance FilmPage styles for mobile and tablet responsiveness

--- src/pages/film/ui/FilmPage.tsx
+++ src/pages/film/ui/FilmPage.tsx
@@ -30,6 +30,7 @@ export function FilmPage() {
       container
       flexDirectionColumn
       justifyBetween
+      alignCenter
       className={styles.container}
     >
       <PageHeader positionAbsolute />
