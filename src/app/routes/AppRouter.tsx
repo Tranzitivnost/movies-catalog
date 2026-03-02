0b5374e59e80fbe0cc8e9701f9ec935e618592d0
tranzitivnost <alevtinapatkina@gmail.com>
2026-03-02 17:02:43 +0100

    feat: implement NotFound page and update routing to handle 404 errors

--- src/app/routes/AppRouter.tsx
+++ src/app/routes/AppRouter.tsx
@@ -1,5 +1,5 @@
 import {
-  createHashRouter,
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
@@ -8,6 +8,7 @@ import { HomePage } from "@pages/home";
 import { ContactPage } from "@pages/contact";
 import { Routes } from "@shared/routes";
 import { FilmPage } from "@/pages/film";
+import { NotFound } from "@/pages/error";
 
 export const AppRouter = () => {
   const routers = createRoutesFromElements(
@@ -15,10 +16,11 @@ export const AppRouter = () => {
       <Route path={Routes.HOME} element={<HomePage />} />,
       <Route path={Routes.CONTACT} element={<ContactPage />} />,
       <Route path={Routes.FILM} element={<FilmPage />} />,
+      <Route path="*" element={<NotFound />} />
     </>,
   );
 
-  const router = createHashRouter(routers, {});
+  const router = createBrowserRouter(routers, {});
 
   return <RouterProvider router={router} />;
 };
